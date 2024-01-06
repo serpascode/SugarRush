1476df914c07b37d7093568a3df11283b7150455
Luis Serpas <serpas.icode@gmail.com>
2024-01-06 09:59:53 -0500

    Jdasiii - Removed missing function input to clear error, added jump variables

--- Assets/Scripts/MovePuff.cs
+++ Assets/Scripts/MovePuff.cs
@@ -4,7 +4,7 @@ using UnityEngine.InputSystem;
 public class MovePuff : MonoBehaviour
 {
     [SerializeField]
-    private float speed = .06f; //Used to control speed of Puff
+    private float speed = .05f; //Used to control speed of Puff
 
     [SerializeField]
     private InputActionReference move;
@@ -14,43 +14,40 @@ public class MovePuff : MonoBehaviour
 
     private Vector2 _moveInput;
 
+    private CharacterController _characterController;
+    // Jump Variables
+    private Vector3 _playerVelocity;
+
+    private bool _groundedPlayer;
+
+    private float _jumpHeight = 5.0f;
+
+    private bool _jumpPressed = false;
+
+    private float _gravityValue = -9.81f;
+
+    // Start is called before the first frame update
+    void start()
+    {
+        _characterController = GetComponent < CharacterController>();
+    }
     // Update is called once per frame
     void Update()
     {
-        //updateMovement();
         _moveInput = move.action.ReadValue<Vector2>();
-    }
-
-    void updateMovement()
-    {
-        //float xDirection = Input.GetAxis("Horizontal");
-        //float yDirection = Input.GetAxis("Vertical");
-
-        //Vector3 moveDirection = new Vector3(xDirection, 0.0f, yDirection); // Y-Axis is up and down, testing flat movement
-
-        //transform.position += moveDirection*speed;
     }
 
     private void FixedUpdate()
     {
         Vector3 moveDirection = new Vector3(_moveInput.x, 0.0f, _moveInput.y);
-
         transform.position += moveDirection * speed;
     }
 
-    private void OnEnable()
-    {
-        jump.action.started += OnJump;
-    }
 
-    private void OnDisable()
-    {
-        jump.action.started -= OnJump;
-    }
-
-    private void OnJump(InputAction.CallbackContext context)
+    private void OnJump()
     {
         Debug.Log("We have Jumped");
-    }
+        
 
+    }
 }
