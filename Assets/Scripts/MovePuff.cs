133a04a0787cc09bf353dc445bd636ca9c8bd879
STRIKENSUN41 <matthew.wolfe4001@gmail.com>
2024-01-06 00:26:02 -0600

    Implemented the input system package
    Added the input system package to the project.
    Also fixed up the puff script for movement to work with it.
    There is an error when trying to use the jump button saying that the method is not found, but the event still triggers regardless. This will need to be debugged later.

--- Assets/Scripts/MovePuff.cs
+++ Assets/Scripts/MovePuff.cs
@@ -1,22 +1,56 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class MovePuff : MonoBehaviour
 {
-    public float speed = .06f; //Used to control speed of Puff
+    [SerializeField]
+    private float speed = .06f; //Used to control speed of Puff
+
+    [SerializeField]
+    private InputActionReference move;
+
+    [SerializeField]
+    private InputActionReference jump;
+
+    private Vector2 _moveInput;
 
     // Update is called once per frame
     void Update()
     {
-        updateMovement();
+        //updateMovement();
+        _moveInput = move.action.ReadValue<Vector2>();
     }
 
     void updateMovement()
     {
-        float xDirection = Input.GetAxis("Horizontal");
-        float yDirection = Input.GetAxis("Vertical");
+        //float xDirection = Input.GetAxis("Horizontal");
+        //float yDirection = Input.GetAxis("Vertical");
 
-        Vector3 moveDirection = new Vector3(xDirection, 0.0f, yDirection); // Y-Axis is up and down, testing flat movement
+        //Vector3 moveDirection = new Vector3(xDirection, 0.0f, yDirection); // Y-Axis is up and down, testing flat movement
 
-        transform.position += moveDirection*speed;
+        //transform.position += moveDirection*speed;
     }
+
+    private void FixedUpdate()
+    {
+        Vector3 moveDirection = new Vector3(_moveInput.x, 0.0f, _moveInput.y);
+
+        transform.position += moveDirection * speed;
+    }
+
+    private void OnEnable()
+    {
+        jump.action.started += OnJump;
+    }
+
+    private void OnDisable()
+    {
+        jump.action.started -= OnJump;
+    }
+
+    private void OnJump(InputAction.CallbackContext context)
+    {
+        Debug.Log("We have Jumped");
+    }
+
 }
